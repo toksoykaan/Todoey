34929d28e4c50d9ab495a42024d6596e8681dd6a
Kaan TOKSOY <kaantoksoy@Kaans-MacBook-Air.local>
2018-10-28 13:00:46 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kaan TOKSOY on 28.10.2018.
+//  Copyright © 2018 Kaan TOKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
